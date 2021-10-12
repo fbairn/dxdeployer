92db43e3decd6541308e4fe4ddaf79f1c14b5165
Fredrick Bairn <fred@sundae.com>
2021-10-12 15:46:45 -0700

    Switched deployments to use the package XML in the commit.

--- ci.js
+++ ci.js
@@ -2,8 +2,6 @@ const dx = require('./dx'),
     argv = require('minimist')(process.argv.slice(2)),
     jetpack = require('fs-jetpack');
 
-var differ, difFunction;
-
 if (argv.h || argv.help) {
     console.log('DX CI Help');
     console.log('-t, --tag\t\tTag for partial deployment & complete. IE: qa');
@@ -16,8 +14,6 @@ if (argv.h || argv.help) {
 
     console.log('-r, --runtests\t\tTests to run, comma seperated');
     console.log('');
-    // console.log('Example: node ci.js -d ./repo -u qasandbox -r test1,test2');
-    // console.log('Example complete deployment and tag: node ci.js -c -t qa');
     process.exit();
 }
 
@@ -28,21 +24,11 @@ const orgUsername = argv.u || argv.username;
 const tests = argv.r || argv.runtests;
 const testLevel = argv.l || argv.testlevel;
 const check = argv.c || argv.check || false;
-const fullDeploy = argv.f || argv.full || false;
 
-
-try {
-    differ = require('./gitit')(path);
-} catch (error) {
-    errorMesg = error.message;
-}
-
-if (!fullDeploy && !tag) {
-    errorMesg += '-t, --tag\t\tTag is required when doing a partial deployment.';
-}
+const gitter = require('./gitit')(path);
 
 if (!orgUsername) {
-    errorMesg += '-u, --user\t\User is required when doing a deployment.';
+    errorMesg += '-u, --user\tUser is required when doing a deployment.';
 }
 
 if (errorMesg != '') {
@@ -51,77 +37,64 @@ if (errorMesg != '') {
     process.exit(1);
 }
 
-if (fullDeploy) {
-    difFunction = differ.createFullPackageFromLocal
-} else if (argv.remote) {
-    difFunction = differ.createPackageFromRemote
-} else {
-    difFunction = differ.createPackageFromLocal
-}
-
-var buildDeploy = async () => {
+var buildDeploy = () => {
 
     jetpack.remove('temp/');
     jetpack.copy(path + '/force-app', 'temp/force-app', {
         overwrite: true
-    })
+    });
     jetpack.copy(path + '/.forceignore', 'temp/.forceignore', {
         overwrite: true
-    })
+    });
     jetpack.copy(path + '/sfdx-project.json', 'temp/sfdx-project.json', {
         overwrite: true
-    })
-
-    const tagList = await differ.getTags({ match: tag });
-    if (tagList.all.length == 0 && !fullDeploy) {
-        throw 'The tag ' + tag + ' was not found.';
-    }
-    const fileCount = await difFunction(tagList.latest, 'HEAD');
-    console.log('File Count:', fileCount);
-    return fileCount;
-}
+    });
+    jetpack.copy(path + '/sfdx-project.json', 'temp/sfdx-project.json', {
+        overwrite: true
+    });
+    jetpack.copy(path + '/manifest/package.xml', 'temp/package.xml', {
+        overwrite: true
+    });
+};
 
 var completeDeployment = async function () {
     var tagTimeStamp = formatDate(new Date());
-    differ.setTags(tag + '-' + tagTimeStamp);
-}
+    gitter.setTags(tag + '-' + tagTimeStamp);
+};
 
 function formatDate(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var year = date.getFullYear() - 2000;
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    const year = date.getFullYear() - 2000;
 
-    hours = hours % 12;
+    // hours = hours % 12;
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     day = day < 10 ? '0' + day : day;
     month = month < 10 ? '0' + month : month;
-    var strTime = hours + '' + minutes;
-    return year + month + day + strTime;
+    const strTime = hours + '' + minutes;
+    console.log(`${year}${month}${day}-${strTime}`);
+    return `${year}${month}${day}-${strTime}`;
 }
 
 console.log('Deploy');
 const rundeploy = async () => {
     try {
-        const fileCount = await buildDeploy();
-        if (fileCount == 0) {
-            console.info('No files to deploy');
-            jetpack.remove('temp/');
-            process.exit(0);
-        }
-
+        buildDeploy();
         await dx.deployMetadata({ orgUsername, testLevel, tests, check });
+        console.log(tag);
+        if (tag) await completeDeployment();
     } catch (error) {
+        jetpack.remove('temp/');
         console.error('Error deploying', error);
         process.exit(1);
     }
-    // await completeDeployment();
     jetpack.remove('temp/');
     console.log('Exiting ');
     process.exit(0);
-}
+};
 rundeploy();
 
 function wait() {
--- gitit/differ.js
+++ gitit/differ.js
@@ -12,31 +12,8 @@
         this._baseDir = baseDir;
 
         this.simpleGit = require('simple-git')(baseDir);
-        this.packageBuilder = require('./packageBuilder');
 
         differ = this;
-    }
-
-
-    Differ.prototype.createPackageFromLocal = async (fromBranch, toBranch) => {
-        const gdiff = await getDiff(fromBranch, toBranch);
-        const pack = await differ.packageBuilder.createPackage(gdiff);
-        return pack;
-    }
-
-    Differ.prototype.createFullPackageFromLocal = async (h) => {
-        const pack = await differ.packageBuilder.createFullPackage();
-        return pack;
-    }
-
-    Differ.prototype.createPackageFromRemote = function (fromBranch, toBranch) {
-        return new Promise(function (fulfill, reject) {
-            pullFromBranch(fromBranch, toBranch)
-                .then(getDiff)
-                .then(differ.packageBuilder.createPackage)
-                .then(fulfill)
-                .catch(reject)
-        });
     }
 
     Differ.prototype.getTags = function (options) {
@@ -44,7 +21,6 @@
         if (options && options.head === true) {
             opts = { '--points-at': 'HEAD' };
         }
-        //options.push('qa-*');
         return new Promise(function (fulfill, reject) {
             differ.simpleGit.tags(opts, (err, tags) => {
                 if (err) return reject(err);
@@ -56,7 +32,7 @@
                 }
                 tags.all.sort();
                 if (tags.all.length > 0) tags.latest = tags.all[tags.all.length - 1];
-                fulfill(tags)
+                fulfill(tags);
             });
         });
     };
@@ -66,38 +42,9 @@
             differ.simpleGit.addAnnotatedTag(tag, '', (err, d) => {
                 if (err) return reject(err);
                 return fulfill(d);
-            })
-        })
-    };
-
-    var getDiff = function (fromBranch, toBranch) {
-        return new Promise(function (fulfill, reject) {
-            //git diff qa-1702271010 head --name-only --diff-filter=AM
-            differ.simpleGit.diffSummary([fromBranch, toBranch, '--diff-filter=AM'], function (err, roger) {
-                if (err) {
-                    reject(err);
-                } else {
-                    fulfill(roger);
-                }
             });
         });
     };
-
-
-    var pullFromBranch = function (fromBranch, toBranch) {
-        return new Promise(function (fulfill, reject) {
-            differ.simpleGit.fetch()
-                .checkout(toBranch)
-                .checkout(fromBranch)
-                .pull(function (err, data) {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        fulfill(fromBranch, toBranch, data);
-                    }
-                });
-        });
-    }
 
     module.exports = Differ;
 }());
